f64c92627afe98293e557b4bec138389e534cdd5
Priya Ramesh <ssreepriyaramesh@gmail.com>
2020-01-19 13:37:51 -0500

    adding new file

---
+++ hello.java
@@ -0,0 +1,15 @@
+
+public class hello {
+
+ public static void main(String[] args) {
+  // TODO Auto-generated method stub
+
+  
+  for (int i = 1; i <=10; i++) {
+   System.out.println("Hello world...."+ i);
+  }
+  
+    
+ }
+
+}
